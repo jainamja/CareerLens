8b0da0338ee321ca2ba4fa28cf88cf7c3ac8d5b1
Jainam Jadav <jainamjadav98@gmail.com>
2026-09-13 10:13:56 +0530

    Update build.sh to seed skills and create superuser

--- build.sh
+++ build.sh
@@ -6,3 +6,9 @@ pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
 python manage.py migrate
+python manage.py seed_skills
+
+# Automatically create superuser if environment variables are provided
+if [[ -n "${DJANGO_SUPERUSER_USERNAME}" && -n "${DJANGO_SUPERUSER_PASSWORD}" ]]; then
+    python manage.py createsuperuser --noinput || true
+fi
