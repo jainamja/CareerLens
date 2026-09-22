be6b71c91e339c78791c02c535a92a1b9170448c
Jainam Jadav <jainamjadav98@gmail.com>
2026-09-22 17:14:27 +0530

    Run seed command in build script

--- build.sh
+++ build.sh
@@ -7,6 +7,7 @@ pip install -r requirements.txt
 python manage.py collectstatic --no-input
 python manage.py migrate
 python manage.py seed_skills
+python manage.py seed
 
 # Automatically create superuser if environment variables are provided
 if [[ -n "${DJANGO_SUPERUSER_USERNAME}" && -n "${DJANGO_SUPERUSER_PASSWORD}" ]]; then
